b9d015116597a2627536253ae66169632b3c6f95
Alberto <aarosteguig@gmail.com>
2023-12-18 00:32:24 +0100

    Init, captures channel movements and join/leave from users. Thinking of what DBMS to use (mby SQLite, mysql), should make a cmake

--- main.cpp
+++ main.cpp
@@ -2,11 +2,25 @@
 #include <cstdlib>
 
 const std::string token =
-    "MTE1MjAyOTUyMTk2OTY4MDQwNQ.Gd40-M.selcAmuT6zAQi6g8XkjIAFy9d-UWpp_2TK7af0";
+    "MTE1MjAyOTUyMTk2OTY4MDQwNQ.G-kPZ8.6lDTFBuEFamMwcK0IvgJGe-7qWeDVaOgiDiOBU";
 
 int main() {
   dpp::cluster bot(token);
   bot.on_log(dpp::utility::cout_logger());
+  bot.on_voice_state_update([](const dpp::voice_state_update_t &event) {
+    if (event.state.channel_id == 0) {
+      std::cout << "User left a vc" << std::endl;
+    } else {
+      std::cout << "Voice state update, user_id: " << event.state.user_id
+                << std::endl;
+      std::cout << "Voice state update, channel_id: " << event.state.channel_id
+                << std::endl;
+      std::cout << "Voice state update, guild_id (server_id) "
+                << event.state.guild_id << std::endl;
+      std::cout << "Voice state update, session_id: " << event.state.session_id
+                << std::endl;
+    }
+  });
   bot.on_slashcommand([](const dpp::slashcommand_t &event) {
     if (event.command.get_command_name() == "ping") {
       event.reply("Pong!");
